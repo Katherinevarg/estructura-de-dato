ea79376164bb52b156938de7b94046879e3a461d
Katherinevarg <159299712+Katherinevarg@users.noreply.github.com>
2024-03-31 08:56:21 -0400

    Create cadenas .h

---
+++ cadenas .h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Cadenas
+{
+private:
+	int longitud;
+	string cadenas;
+public:
+	Cadenas();
+	~Cadenas();
+	string get_Cadenas();
+	void set_cadenas();
+	int tamanio();
+	int substring();
+	string caracter();
+	//colocar el resto de metodos
+};
